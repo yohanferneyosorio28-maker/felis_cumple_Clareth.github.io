06d47f578d716631ed3eb82de3ff7c44199703ca
yohanferneyosorio28-maker <yohanferneyosorio28@gmail.com>
2026-06-08 14:48:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function abrirCarta(){
     document.getElementById("sobre").innerHTML = "💌";
 
 }
+
+function crearFlor(){
+
+    const flor = document.createElement("div");
+
+    flor.classList.add("flor");
+
+    const flores = ["🌸","🌺","🌷"];
+
+    flor.innerHTML =
+        flores[Math.floor(Math.random()*flores.length)];
+
+    flor.style.left =
+        Math.random()*window.innerWidth + "px";
+
+    flor.style.animationDuration =
+        (5 + Math.random()*5) + "s";
+
+    document.getElementById("flores")
+        .appendChild(flor);
+
+    setTimeout(() => {
+        flor.remove();
+    },10000);
+
+}
+
+setInterval(crearFlor,400);
